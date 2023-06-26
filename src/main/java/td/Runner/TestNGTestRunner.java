abc2dcd944cb5242d385f4107c0df93428f805d5
Gayathiridevi <pgayathiridevi94@gmail.com>
2023-06-26 14:38:37 -0400

    Step Definition for home page and Runner class added

---
+++ src/main/java/td/Runner/TestNGTestRunner.java
@@ -0,0 +1,20 @@
+package td.Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(
+		features = "C:\\Users\\harih\\eclipse-workspace\\TDCucumberFramework\\src\\main\\java\\td\\Features\\tdhome.feature",
+		glue="td.StepDefinitions",
+		plugin = "pretty"
+		
+		
+		
+		)
+
+
+
+public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+
+}
---
+++ src/main/java/td/StepDefinitions/HomeSteps.java
@@ -0,0 +1,22 @@
+package td.StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class HomeSteps {
+	
+	@Given("^User is on Home Page$")
+	public void user_on_homepage() {
+		
+	}
+	@Then("")
+	public void user_enters_text_in_search_box() {
+	    
+	}
+	@And("")
+	public void user_clicks_on_search_button() {
+	    
+	}
+
+}
